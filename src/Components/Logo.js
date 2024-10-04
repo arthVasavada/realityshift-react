0b011f41c55828974ada3ed1939c4704e16e08c8
thetransientone <arth.vasavada@gmail.com>
2024-10-04 15:51:59 -0700

    Added logo

---
+++ src/Components/Logo.js
@@ -0,0 +1,8 @@
+import logo from "../Media/Logo_Main.jpg";    
+
+const Logo = (props) =>{
+    const incLogo = <img src={logo} alt="RealityShift Logo"/>;
+    return incLogo;
+} 
+
+export default Logo;
